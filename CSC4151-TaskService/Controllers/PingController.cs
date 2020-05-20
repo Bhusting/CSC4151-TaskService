9f6e29ded395f7882dedbbf7855d8aecbb25373d
Bhusting97 <bhusting@spu.edu>
2020-05-20 13:22:33 -0700

    Added Ping Controller

---
+++ CSC4151-TaskService/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSC4151_TaskService.Controllers
+{
+    [ApiController]
+    [Route("Ping")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Ping()
+        {
+            return "Task Pong";
+        }
+    }
+}
